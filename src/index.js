98d2fc1798a40ad8ed4a8232ecedea011a1a3d4d
agustinpardolp <agustinpardo@hotmail.com>
2020-03-25 09:07:30 -0300

    configuracion webpack ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "./index.css";
 import Main from './Main';
 import * as serviceWorker from './serviceWorker';
 
